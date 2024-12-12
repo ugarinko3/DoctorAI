6ff59fc8da85be076b9ab99ee007468ec35d7d18
ugarinko3 <ugarinko3@icloud.com>
2024-12-12 20:24:01 +0700

    start

---
+++ src/main/java/com/example/doctorai/DoctorAiApplication.java
@@ -0,0 +1,13 @@
+package com.example.doctorai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoctorAiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/doctorai/DoctorAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.doctorai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctorAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
